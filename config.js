c466a060e3c070b162a7315c3068db711e656fb0
Jorgeurbakasa <jcortes@urbakasa.com>
2026-07-27 18:50:10 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Reemplaza estos dos valores con los de tu proyecto Supabase
+// (Project Settings -> API en el dashboard de Supabase)
+const SUPABASE_URL = "https://tklfbgcczqepuadrhvth.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrbGZiZ2NjenFlcHVhZHJodnRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxMDg2MTAsImV4cCI6MjEwMDY4NDYxMH0.Z7i4TNnMNvUAZQEB81cmvsAMytUDtmiG27r_b4SGQM8";
